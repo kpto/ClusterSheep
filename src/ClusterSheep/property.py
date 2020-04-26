a040a80799b6da907a18e7a1ac3cbebeda2b3644
Paul TO <kpto@connect.ust.hk>
2020-04-27 00:19:06 +0800

    test

--- src/ClusterSheep/property.py
+++ src/ClusterSheep/property.py
@@ -25,7 +25,7 @@ import numpy as np
 
 
 # ====BEGIN OF GLOBAL VARIABLE DECLARATION====
-VERSION = '0.90.4'
+VERSION = '20.04.0'
 NAME = 'ClusterSheep'
 
 # size of the header of customized file format for metadata
