193c8b13f46e307433f685b46854833e9416ae99
Paul TO <kpto@connect.ust.hk>
2020-04-27 04:46:15 +0800

    Deleted file

--- src/ClusterSheep/property.py
+++ src/ClusterSheep/property.py
@@ -25,7 +25,7 @@ import numpy as np
 
 
 # ====BEGIN OF GLOBAL VARIABLE DECLARATION====
-VERSION = '20.04.4'
+VERSION = '20.04.5'
 NAME = 'ClusterSheep'
 
 # size of the header of customized file format for metadata
