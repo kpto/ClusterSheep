25c1272f009deaf50794da12e5086918484ce140
Paul TO <kpto@connect.ust.hk>
2020-04-27 04:24:11 +0800

    Added pipeline for release branch. Added more tags in setup.py classifier.

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Programming Language :: C',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
@@ -85,6 +86,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Operating System :: POSIX :: Linux',
         'Topic :: Scientific/Engineering :: Bio-Informatics'
     ]
 )
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,2 +1,5 @@
-def test_test():
+def test_test1():
+    assert True is True
+
+def test_test2():
     assert True is True
